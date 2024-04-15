de14484394385e905357e89e3dd0251b26be21c4
Jaimes Cayetano <jhonalexisjaimescayetano1@gmail.com>
2024-04-14 21:14:16 -0500

    add messages returned by validation in forms

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,20 +5,25 @@
         <form action="{{ route('login.store') }}" method="POST" class="form">
             @csrf
             <h1>
-                  Login
+                Login
             </h1>
 
             <label for="email">Email</label>
-            <input required class="input" type="email" name="email" id="email">
+            <input class="@error('email') is-invalid @enderror input" type="email" name="email" id="email">
+            @error('email')
+                <div class="error-message">{{ $message }}</div>
+            @enderror
 
             <label for="password">Password</label>
-            <input required class="input" type="password" name="password" id="password">
-            <div class="error-message">Fields is invalid</div>
+            <input class="@error('email') is-invalid @enderror input" type="password" name="password" id="password">
+            @error('password')
+                <div class="error-message">{{ $message }}</div>
+            @enderror
+
 
             <input class="button" type="submit" value="Login">
-
             <p>
-                  Don't have an account? <a href="{{ route('register.store') }}">Register</a>
+                Don't have an account? <a href="{{ route('register.store') }}">Register</a>
             </p>
         </form>
     </div>
