cc5f535c583dc402c6e0d8aeb92cca55a5478b84
Alexandra Vegas <alexvegasdev@gmail.com>
2024-04-15 20:17:20 -0500

    merge

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 });
 
 
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index'])->name('homepage');
 
 Route::group([
     'middleware' => 'guest'
