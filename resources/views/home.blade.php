738f1a19bfddc5bdfc47499416e2b69e4cc7223f
Jaimes Cayetano <jhonalexisjaimescayetano1@gmail.com>
2024-04-15 19:48:39 -0500

    create reusable navbar component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Home</title>
-</head>
-<body>
+@extends('partials.head')
+
+@section('content')
+    <section style="margin-top: 60px;">
       <h1>Home</h1>
 
       @auth
-            <p>Welcome</p>
-            <p>
-                  <a href="/logout">Logout</a>
-            </p>
-
+          <p>Welcome</p>
+          <p>
+              <a href="/logout">Logout</a>
+          </p>
+  
       @endauth
-
+  
       @guest
-            <p>
-                  <a href="/login">Login</a>
-            </p>
+          <p>
+              <a href="/login">Login</a>
+          </p>
       @endguest
-
-</body>
-</html>
+    </section>
+@endsection
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,30 @@
     <title>City Tours</title>
     @vite(['resources/css/app.css'])
 </head>
+
 <body>
+    <header class="header-fixed">
+        <nav class="nav-bar">
+            <a href="{{ route('homepage') }}" class="nav-logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"
+                    class="icon icon-tabler icons-tabler-outline icon-tabler-plane-tilt">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path
+                        d="M14.5 6.5l3 -2.9a2.05 2.05 0 0 1 2.9 2.9l-2.9 3l2.5 7.5l-2.5 2.55l-3.5 -6.55l-3 3v3l-2 2l-1.5 -4.5l-4.5 -1.5l2 -2h3l3 -3l-6.5 -3.5l2.5 -2.5l7.5 2.5z" />
+                </svg>
+                <span>City Tours</span>
+            </a>
+            <div class="nav-redirects">
+                <a href="{{ route('homepage') }}">Home</a>
+                <a href="#">Categories</a>
+                <a href="#">Packages</a>
+                <a href="#">Places</a>
+                <a href="{{ route('login.show') }}">Login</a>
+            </div>
+        </nav>
+    </header>
     @yield('content')
 </body>
+
 </html>
